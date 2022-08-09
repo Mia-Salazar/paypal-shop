d11d570821adedde382d2f9b2e7fa7c0fb846d3c
Mia-Salazar <maria.sgr@gmail.com>
2022-08-09 20:32:17 +0200

    Add checkout styles

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -4,7 +4,24 @@ import "./Checkout.css";
 
 const Checkout = () => {
   return (
-    <h1>Checkout</h1>
+    <section className="Checkout">
+      <div className="Checkout-content">
+        <h2>Lista de Pedidos:</h2>
+        <ul>
+          <li className="Checkout-item">
+            <div className="Checkout-element">
+              <h3>ITEM name</h3>
+              <span>$10</span>
+            </div>
+            <button type="button">Eliminar</button>
+          </li>
+        </ul>
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Precio Total: $10</h3>
+        <button type="button">Continuar pedido</button>
+      </div>
+    </section>
   );
 }
 
