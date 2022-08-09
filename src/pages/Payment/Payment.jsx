9bee9aafaa22df25970c5f1220cc841ccb84d47e
Mia-Salazar <maria.sgr@gmail.com>
2022-08-09 20:42:25 +0200

    Add payment page and styles

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -4,7 +4,15 @@ import "./Payment.css";
 
 const Payment = () => {
   return (
-    <h1>Payment</h1>
+    <section className="Payment">
+      <div className="Payment-content">
+        <h3>Resument del pedido:</h3>
+        <button className="Payment-button">
+          Boton de pago con Paypal
+        </button>
+      </div>
+      <div />
+    </section>
   );
 }
 
