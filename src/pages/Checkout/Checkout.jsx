b4902df838e97858093cb125820e16cb1c3735ce
Mia-Salazar <maria.sgr@gmail.com>
2022-08-10 19:04:37 +0200

    Checkout items and remove items

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import AppContext from '../../context/AppContext';
 import "./Checkout.css";
 
 const Checkout = () => {
+  const { state, removeFromCart } = useContext(AppContext);
+  const { cart } = state;
+
+  const handleRemove = product => () => {
+    removeFromCart(product);
+  };
+
+  const handleSumTotal = () => {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  }
+
   return (
     <section className="Checkout">
       <div className="Checkout-content">
-        <h2>Lista de Pedidos:</h2>
+        {cart.length > 0  ?<h2>Lista de Pedidos:</h2> : <h2>No hay productos añadidos</h2>}
         <ul>
-          <li className="Checkout-item">
-            <div className="Checkout-element">
-              <h3>ITEM name</h3>
-              <span>$10</span>
-            </div>
-            <button type="button">Eliminar <i className="fas fa-trash-alt" title="Eliminar" /></button>
-          </li>
+          {cart.map((item) => (
+            <li className="Checkout-item">
+              <div className="Checkout-element">
+                <h3>{item.title}</h3>
+                <span>{item.price}€</span>
+              </div>
+              <button type="button" onClick={handleRemove(item)}>
+                Eliminar
+                <i className="fas fa-trash-alt" />
+              </button>
+            </li>
+          ))}
+
         </ul>
       </div>
+      {cart.length > 0 &&
       <div className="Checkout-sidebar">
-        <h3>Precio Total: $10</h3>
+        <h3>{`Precio Total: ${handleSumTotal()}€`}</h3>
         <Link to="/checkout/information">
           Continuar pedido
         </Link>
-      </div>
+      </div>}
     </section>
   );
 }
