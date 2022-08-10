11103038dce47c1433e428b4604e2c685007d88e
Mia-Salazar <maria.sgr@gmail.com>
2022-08-10 18:30:55 +0200

    Add useContext and addtocart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import AppContext from '../../context/AppContext';
 import './Header.css'
 
 const Header = () => {
+  const { state } = useContext(AppContext);
+  const { cart } = state;
+
   return (
     <div className="Header">
       <h1 className="Header-title">
@@ -12,6 +16,8 @@ const Header = () => {
       <div className="Header-checkout">
         <Link to="/checkout">
           <i className="fas fa-shopping-basket" />
+          <span className="Header-alert">{cart.length}</span>
+          
         </Link>
       </div>
     </div>
